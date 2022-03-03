889a696c97423ae57873cda977e367357182088e
Edito Donato <editotchocosso13@gmail.com>
2022-03-03 11:14:24 +0100

    Inserindo de vez o Slogan na master

---
+++ src/Components/Slogan/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from './style'
+
+const index = () => {
+  return (
+      <Container>
+        <div>
+          <h4>Que <span>delicatezza!</span></h4>
+          <p>Se é delicioso, está aqui!</p>
+        </div>
+      </Container>
+  )
+}
+
+export default index
---
+++ src/Components/Slogan/style.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+import Back_slogan from '../../Imagens/banner_slogan.png'
+
+export const Container = styled.div`
+    background: url('${Back_slogan}');
+    background-position: center;
+    background-size: cover;
+
+    & div{
+        background: linear-gradient(to bottom, #000000c3 75%, transparent);
+        padding: 40px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 5px;
+        color: #eee;
+       
+
+        h4{
+            font-weight: 400;
+            font-size: 3em;
+            font-family: 'Krinkes Decor PERSONAL USE';
+            span{
+                color: #FF00E8;
+                font-family: 'Krinkes Decor PERSONAL USE';
+            }
+        }
+
+        p{
+            font-family: 'Roboto Light';
+            font-size: 1.6em;
+        }
+    }
+`;
