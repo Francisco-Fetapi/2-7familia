6af5e5afd2ea2d68ac3396b1dc07f11798573157
Edito Donato <editotchocosso13@gmail.com>
2022-03-11 15:18:58 +0100

    Criando o estados para os inputs e suas mudanças no Login do Admin

--- src/Pages/Admin/Login/index.jsx
+++ src/Pages/Admin/Login/index.jsx
@@ -10,36 +10,48 @@ import { Container } from './style'
 export default () => {
 
   const [Login, setLogin] = useState(true);
+  const [Campos, setCampos] = useState({
+    email: '',
+    senha: ''
+  });
+
+  const handleChange = e => {
+    const campo = e.target
+    setCampos({
+      ...Campos,
+      [campo.name]: campo.value
+    })
+  } 
 
   const Logar = e => {
     e.preventDefault()
   }
 
   return (
-      <Container>
-        {
-          Login ? (
-            <>
+    <Container>
+      {
+        Login ? (
+          <>
             <h2>Logar como Admin</h2>
-              <form onSubmit={Logar}>
-                <div>
-                  <label htmlFor="email">E-mail</label>
-                  <input type="text" name='email' placeholder='exemplo@gmail.com' id='email'/>
-                </div>
-                <div>
-                  <label htmlFor="senha">Senha</label>
-                  <input type="password" name='senha' placeholder='editoh$20/%' id='senha'/>
-                </div>
-                <Button className='button'>Logar</Button>
-              </form>
-              <Tooltip title={<Title>Crie sua conta</Title>} arrow>
-                <p onClick={() => setLogin(false)}>Não tem uma conta? <span>Criar.</span></p>
-              </Tooltip>
-            </>
-          )
-          : <Cadastro voltar={setLogin}/>
-        }
-      </Container>
-      
+            <form onSubmit={Logar}>
+              <div>
+                <label htmlFor="email">E-mail</label>
+                <input type="text" name='email' value={Campos.email} onChange={handleChange} placeholder='exemplo@gmail.com' id='email' />
+              </div>
+              <div>
+                <label htmlFor="senha">Senha</label>
+                <input type="password" name='senha' value={Campos.senha} onChange={handleChange} placeholder='editoh$20/%' id='senha' />
+              </div>
+              <Button className='button'>Logar</Button>
+            </form>
+            <Tooltip title={<Title>Crie sua conta</Title>} arrow>
+              <p onClick={() => setLogin(false)}>Não tem uma conta? <span>Criar.</span></p>
+            </Tooltip>
+          </>
+        )
+          : <Cadastro voltar={setLogin} />
+      }
+    </Container>
+
   )
 }
--- src/Pages/Admin/index.jsx
+++ src/Pages/Admin/index.jsx
@@ -1,40 +1,43 @@
 import React, { useState } from 'react'
 
 import { Button } from '@material-ui/core'
-import Produtos from  './Produtos'
+import Produtos from './Produtos'
 import { Container, Conteudo, Drawer } from './style'
 import avatar from '../../Imagens/avatar.png'
-import { BusinessCenter, Chat, ChatBubble, PeopleAlt, Shop, ShoppingCart } from '@material-ui/icons'
+import { BusinessCenter, Chat, PeopleAlt, Shop, ShoppingCart } from '@material-ui/icons'
 
 const Index = () => {
-
     const [Item, setItem] = useState('dashboard');
 
-    return (
-        <Container>
-            <Drawer>
-                <div>
-                    <img src={avatar} alt="Avatar" />
-                    <p>Editoh Donatello</p>
-                </div>
-                <Button style={{marginTop: '40px'}} onClick={() => setItem('dashboard')} fullWidth><BusinessCenter/> Dashboard</Button>  
-                <Button onClick={() => setItem('produtos')} fullWidth><Shop /> Produtos</Button>  
-                <Button onClick={() => setItem('encomendas')} fullWidth><ShoppingCart /> Encomendas</Button>  
-                <Button onClick={() => setItem('usuarios')} fullWidth><PeopleAlt /> Usuários</Button>  
-                <Button onClick={() => setItem('chat')} fullWidth><Chat /> Chat</Button>  
-            </Drawer>
-            <Conteudo>
-                <h1>Painel Administrador</h1>
-                {
-                    Item === 'dashboard' ? <h2>Dashboard</h2>
-                    : Item === 'produtos' ? <Produtos />
-                        : Item === 'encomendas' ? <h2>Encomendas</h2>
-                            : Item === 'usuarios' ? <h2>Usuários</h2>
-                                : Item === 'chat' ? <h2>Chat</h2> : ''
-                }
-            </Conteudo>
-        </Container>
-    )
+    if (!localStorage.admin_logado) window.location.href = 'http://localhost:3000/admin/login'
+
+    else {
+        return (
+            <Container>
+                <Drawer>
+                    <div>
+                        <img src={avatar} alt="Avatar" />
+                        <p>Editoh Donatello</p>
+                    </div>
+                    <Button style={{ marginTop: '40px' }} onClick={() => setItem('dashboard')} fullWidth><BusinessCenter /> Dashboard</Button>
+                    <Button onClick={() => setItem('produtos')} fullWidth><Shop /> Produtos</Button>
+                    <Button onClick={() => setItem('encomendas')} fullWidth><ShoppingCart /> Encomendas</Button>
+                    <Button onClick={() => setItem('usuarios')} fullWidth><PeopleAlt /> Usuários</Button>
+                    <Button onClick={() => setItem('chat')} fullWidth><Chat /> Chat</Button>
+                </Drawer>
+                <Conteudo>
+                    <h1>Painel Administrador</h1>
+                    {
+                        Item === 'dashboard' ? <h2>Dashboard</h2>
+                            : Item === 'produtos' ? <Produtos />
+                                : Item === 'encomendas' ? <h2>Encomendas</h2>
+                                    : Item === 'usuarios' ? <h2>Usuários</h2>
+                                        : Item === 'chat' ? <h2>Chat</h2> : ''
+                    }
+                </Conteudo>
+            </Container>
+        )
+    }
 }
 
 export default Index
