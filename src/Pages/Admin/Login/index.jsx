fc09bf2efa8c5a09c0e3faee92d66902be1679cd
Edito Donato <editotchocosso13@gmail.com>
2022-03-11 15:51:44 +0100

    Adicionando Popover na página de login do admin para dicas

--- src/Pages/Admin/Login/index.jsx
+++ src/Pages/Admin/Login/index.jsx
@@ -1,80 +1,133 @@
 /* eslint-disable no-throw-literal */
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import { Tooltip } from '@material-ui/core';
-import Title from '../../../Components/TitleTooltip';
-import Button from '../components/button';
-import Cadastro from './cadastro';
-import { Container } from './style'
-import API from '../../../_config/API';
+import { Popover, Tooltip } from "@material-ui/core";
+import Title from "../../../Components/TitleTooltip";
+import Button from "../components/button";
+import Cadastro from "./cadastro";
+import { Container } from "./style";
+import API from "../../../_config/API";
+import { Certo, Errado } from "../../Login/style2";
+import { CheckCircleOutlined } from "@material-ui/icons";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-
   const [Login, setLogin] = useState(true);
   const [Campos, setCampos] = useState({
-    email: '',
-    codigo: ''
+    email: "",
+    codigo: "",
   });
 
-  const handleChange = e => {
-    const campo = e.target
+  const [Erro, setErro] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(false);
+
+    // Activar Popover
+    const handleClick = () => {
+        setAnchorEl(true);
+        setTimeout(() => handleClosePop(), 1300)
+    };
+
+    // Desactivar Popover
+    const handleClosePop = () => setAnchorEl(false)
+
+    const id = anchorEl ? 'simple-popover' : undefined;
+
+  const handleChange = (e) => {
+    const campo = e.target;
     setCampos({
       ...Campos,
-      [campo.name]: campo.value
-    })
-  } 
+      [campo.name]: campo.value,
+    });
+  };
 
-  const Logar = async e => {
-    e.preventDefault()
-    
-    const FD = new FormData()
+  const Logar = async (e) => {
+    e.preventDefault();
+
+    const FD = new FormData();
 
     try {
-      if(Campos.email === '') throw 'E-mail está vazio'
-      else if(Campos.codigo === '') throw 'Senha está vazia'
+      if (Campos.email === "") throw "E-mail está vazio";
+      else if (Campos.codigo === "") throw "Senha está vazia";
 
-      FD.append('email',Campos.email)
-      FD.append('codigo',Campos.codigo)
+      FD.append("email", Campos.email);
+      FD.append("codigo", Campos.codigo);
 
-      const response = await API.verificar_login_admin(FD)
+      const response = await API.verificar_login_admin(FD);
 
-      if(response === 'Admin não encontrado') alert(response)
-      else{
-        localStorage.setItem('admin_logado', response)
-        window.location.href = 'http://localhost:3000/admin'
+      if (response === "Admin não encontrado") setErro(response);
+      else {
+        localStorage.setItem("admin_logado", response);
+        handleClick()
       }
-
     } catch (error) {
-      alert(error)
+      setErro(error);
+      handleClick()
+    }finally{
+      window.location.href = "http://localhost:3000/admin";
     }
-  }
+  };
 
   return (
     <Container>
-      {
-        Login ? (
-          <>
-            <h2>Logar como Admin</h2>
-            <form onSubmit={Logar}>
-              <div>
-                <label htmlFor="email">E-mail</label>
-                <input type="email" name='email' value={Campos.email} onChange={handleChange} placeholder='exemplo@gmail.com' id='email' />
-              </div>
-              <div>
-                <label htmlFor="senha">Código de acesso</label>
-                <input type="password" name='codigo' value={Campos.codigo} onChange={handleChange} placeholder='editoh$20/%' id='senha' />
-              </div>
-              <Button className='button'>Logar</Button>
-            </form>
-            <Tooltip title={<Title>Crie sua conta</Title>} arrow>
-              <p onClick={() => setLogin(false)}>Não tem uma conta? <span>Criar.</span></p>
-            </Tooltip>
-          </>
-        )
-          : <Cadastro voltar={setLogin} />
-      }
+      {Login ? (
+        <>
+          <h2>Logar como Admin</h2>
+          <form onSubmit={Logar}>
+            <div>
+              <label htmlFor="email">E-mail</label>
+              <input
+                type="email"
+                name="email"
+                value={Campos.email}
+                onChange={handleChange}
+                placeholder="exemplo@gmail.com"
+                id="email"
+              />
+            </div>
+            <div>
+              <label htmlFor="senha">Código de acesso</label>
+              <input
+                type="password"
+                name="codigo"
+                value={Campos.codigo}
+                onChange={handleChange}
+                placeholder="editoh$20/%"
+                id="senha"
+              />
+            </div>
+            <Button className="button">Logar</Button>
+          </form>
+          <Tooltip title={<Title>Crie sua conta</Title>} arrow>
+            <p onClick={() => setLogin(false)}>
+              Não tem uma conta? <span>Criar.</span>
+            </p>
+          </Tooltip>
+          <Popover
+            id={id}
+            open={anchorEl}
+            anchorEl={anchorEl}
+            onClose={handleClosePop}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            transformOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+          >
+            {Erro ? (
+              <Errado>Erro: {Erro}!</Errado>
+            ) : (
+              <Certo>
+                <CheckCircleOutlined /> Login feito!
+              </Certo>
+            )}
+          </Popover>
+        </>
+      ) : (
+        <Cadastro voltar={setLogin} />
+      )}
     </Container>
-
-  )
-}
+  );
+};
