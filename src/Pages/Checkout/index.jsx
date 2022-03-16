4fa518ef72f70297adcd86f11736383ca765c616
Edito Donato <editotchocosso13@gmail.com>
2022-03-16 09:43:52 +0100

    Adicionando a rota para a page de Checkout

---
+++ src/Pages/Checkout/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <h1>Checkout</h1>
+  )
+}
+
+export default Index
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 import Home from './Pages/Home/'
 import Sobre from './Pages/Sobre'
 import Produtos from './Pages/Produtos'
+import Checkout from './Pages/Checkout'
 import Contacto from './Pages/Contacto'
 import Login from './Pages/Login/Index'
 import LoginAdmin from './Pages/Admin/Login/'
@@ -21,7 +22,7 @@ const Rotas = () => (
                 <Route path='/sobre' component={Sobre} />
                 <Route path='/contacto' component={Contacto} />
                 <Route path='/produtos' component={Produtos} />
-                <Route path='/encomendar/:id_produto' component={Produtos} />
+                <Route path='/encomendar/:id_produto' component={Checkout} />
                 <Route path='/login' component={Login}/>
                 <Route path='/admin/login' component={LoginAdmin}/>
                 <Route path='/admin' component={PainelAdmin}/>
