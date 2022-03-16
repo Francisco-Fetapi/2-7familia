fe019a26d58bd9fb92b9defedc741d6509e52719
Edito Donato <editotchocosso13@gmail.com>
2022-03-16 09:35:29 +0100

    Adicionando o modal Adoros a master

---
+++ src/Components/ModalAdoros/index.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+
+import { Dialog, IconButton } from '@material-ui/core'
+import { Container, ImagemProduto, ItemProduto, Title } from './style'
+import API from '../../_config/API'
+import { DeleteForever, ShopTwo } from '@material-ui/icons'
+
+const Index = ({ open, handleClose, Reacoes, desReagir }) => {
+
+    const user_logado = +localStorage.usuario_logado
+
+    useEffect(() => {
+        buscaProdutosAdorados(user_logado)
+    }, [Reacoes]);
+
+    const [ProdutosAdorados, setProdutosAdorados] = useState([]);
+
+    const buscaProdutosAdorados = async id_usuario => {
+        const response = await API.selecionar_produtos_adorados({ id_usuario })
+        setProdutosAdorados(response)
+    }
+
+
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+        {
+            ProdutosAdorados.length ? (
+                <Container>
+                    <Title><ShopTwo /> Produtos Adorados</Title>
+                    <p style={{marginBottom: '4%'}}>{ProdutosAdorados.length > 1 ? ProdutosAdorados.length+' Itens adorados' : ProdutosAdorados.length+' Item adorado'}</p>
+                    {
+                        ProdutosAdorados.map(produto => (
+                            <ItemProduto>
+                                <ImagemProduto imagem={`http://localhost:8000/`+produto.produtos.foto_produto}/>
+                                <div>
+                                    <p className='nome_produto'>{produto.produtos.nome_produto.length > 17 ? produto.produtos.nome_produto.substring(0,17)+'...' : produto.produtos.nome_produto}</p>
+                                    <p>KZ {produto.produtos.preco}</p>
+                                </div>
+                                <IconButton onClick={() => desReagir(produto.id)}>
+                                    <DeleteForever/>
+                                </IconButton>
+                            </ItemProduto>
+                        ))
+                    }
+                </Container>
+            ) : <p>Carregando</p>
+        }
+    </Dialog>
+  )
+}
+
+export default Index
