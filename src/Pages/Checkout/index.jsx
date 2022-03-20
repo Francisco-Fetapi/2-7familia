04fef5317cd29bdc1a30978d5ffa616a8b997ccf
Edito Donato <editotchocosso13@gmail.com>
2022-03-20 10:41:23 +0100

    Resolvendo o problema de número mínimo para a quantidade de encoomendas

--- src/Pages/Checkout/index.jsx
+++ src/Pages/Checkout/index.jsx
@@ -31,9 +31,37 @@ const Index = () => {
     const response = await API.selecionar_reacoes_produto({ produto_id })
     setReacoesProduto(response)
   }
+  	
+  const date = new Date()
+  const [Campos, setCampos] = useState({
+      quantidade: 1,
+      data_entrega: ''
+  });
+
+  const handleChange = e => {
+    e.preventDefault()
+    const campo = e.target
+
+    setCampos({
+      ...Campos,
+      [campo.name]: campo.value
+    })
+
+    if(Campos.quantidade <= 0) setCampos({...Campos, quantidade: 1})
+  }
 
   const Voltar = () => {
       history.goBack()
+  }
+
+  const encomendar = e => {
+    e.preventDefault()
+    
+    try {
+      // if(Campos.quantidade < 1) throw ''
+    } catch (error) {
+      
+    }
   }
 
   return (
@@ -45,18 +73,18 @@ const Index = () => {
               <h1>Ótima escolha!</h1>
               <p>Obrigado por querer encomendar os nossos produtos! Confirme a sua encomenda.</p>
             </div>
-            <form>
+            <form onSubmit={encomendar}>
               <div className="Item">
                 <ImagemProduto imagem={`http://localhost:8000/`+Produto[0].foto_produto} />
                 <div className="conteudo">
                   <h3><ShoppingCart /> Sua encomenda</h3>
                   <div>
-                    <label htmlFor="">Quantidade:</label>
-                    <input type="number" />
+                    <label htmlFor="quantidade">Quantidade:</label>
+                    <input type="number" name="quantidade" min={1} id="quantidade" value={Campos.quantidade} onChange={handleChange}/>
                   </div>
                   <div>
                     <label htmlFor="data_entrega">Data de Entrega:</label>
-                    <input type="date" id='data_entrega' />
+                    <input type="date" name="data_entrega" id="data_entrega" value={Campos.data_entrega} onChange={handleChange}/>
                   </div>
                 </div>
               </div>
